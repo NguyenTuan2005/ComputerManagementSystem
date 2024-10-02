c64151390aec71daa99ab22bd44d33d343c1bc12
Duy <duynguyenavg@gmail.com>
2024-10-02 17:06:51 +0700

    test

---
+++ src/main/java/view/CustomerPanel.java
@@ -0,0 +1,6 @@
+package view;
+
+import javax.swing.*;
+
+public class CustomerPanel extends JPanel {
+}
---
+++ src/main/java/view/Style.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.awt.*;
+
+public class Style {
+    public static Color BACKGROUND_COLOR = new Color(0, 128, 255, 150);
+    
+    public static Color BACKGROUND_COLOR_DIV2 = new Color(0, 128, 255, 0);
+    
+    public static Font FONT_SIZE = new Font("Arial", Font.PLAIN, 15);
+}

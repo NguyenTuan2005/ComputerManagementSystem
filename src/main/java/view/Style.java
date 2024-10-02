04bc73e420a1f8c7c9efbfe3de359e60f28be057
QTuan <23130370@st.hcmuaf.edu.vn>
2024-10-02 23:16:32 +0700

    Clean code by Tuan

--- src/main/java/view/Style.java
+++ src/main/java/view/Style.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class Style {
     public static Color BACKGROUND_COLOR = new Color(0, 128, 255, 150);
     
-    public static Color BACKGROUND_COLOR_DIV2 = new Color(0, 128, 255, 0);
+    public static Color BACKGROUND_COLOR_FUll_BLUR = new Color(0, 128, 255, 0);
     
     public static Font FONT_SIZE = new Font("Arial", Font.PLAIN, 15);
 }

7837d81e0816b31872916159d9713141d07958e0
HuyHoangPb <huyhoang2875@gmail.com>
2024-10-07 23:41:51 +0700

    feat: add loginframe, userframe,login feature,add missing feature in mainpanel

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -12,9 +12,19 @@ public class User {
     private int id;
     private String userName;
     private String password;
-    private int role;
+    private String role;
     private String email;
     private String address;
+    public static final String MANAGER_ROLE = "Manager";
+    public static final String User_ROLE = "User";
 
-
+    public boolean sameName(String name){
+        return this.userName.equals(name);
+    }
+    public boolean samePassword(String passwd){
+        return this.password.equals(passwd);
+    }
+    public boolean sameRole(String role){
+        return this.role.equals(role);
+    }
 }

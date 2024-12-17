d36d51da5302a28b9fee9c7b72345528427ab86e
HuyHoangPb <huyhoang2875@gmail.com>
2024-12-17 14:46:15 +0700

    update cartPanel

--- src/main/java/view/CustomerMainPanel.java
+++ src/main/java/view/CustomerMainPanel.java
@@ -154,7 +154,7 @@ public class CustomerMainPanel extends JPanel {
                 searchBar.add(searchTextField, gbc);
 
                 searchBt = ButtonConfig.createCustomButton("", Style.FONT_PLAIN_20, Style.WORD_COLOR_WHITE, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, Style.LIGHT_BlUE, 0, SwingConstants.CENTER, new Dimension(50, 40));
-                ButtonConfig.setButtonIcon("src/main/java/Icon/search_Icon.png", searchBt,5);
+                ButtonConfig.setButtonIcon("src/main/java/Icon/search_Icon.png", searchBt, 5);
 
 
                 gbc.gridx = 2;
@@ -336,7 +336,7 @@ public class CustomerMainPanel extends JPanel {
 
                 backBt = ButtonConfig.createCustomButton("Back", Style.FONT_BOLD_16, Color.white, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE,
                         Style.MEDIUM_BLUE, 5, SwingConstants.CENTER, new Dimension(110, 30));
-                ButtonConfig.setButtonIcon("src/main/java/Icon/back_Icon1.png", backBt,5);
+                ButtonConfig.setButtonIcon("src/main/java/Icon/back_Icon1.png", backBt, 5);
                 backBt.addActionListener(e -> {
                     ProductCatalogMainPanel.this.show("catalog");
                 });
@@ -519,20 +519,20 @@ public class CustomerMainPanel extends JPanel {
                 add(summaryPanel, BorderLayout.EAST);
             }
 
-            class ProductsInCartPanel extends JPanel{
+            class ProductsInCartPanel extends JPanel {
 
-                ProductsInCartPanel(){
+                ProductsInCartPanel() {
                     setLayout(new BorderLayout());
 
                     JPanel titlePnTop = new JPanel(new BorderLayout());
                     titlePnTop.setBackground(Color.WHITE);
                     titlePnTop.setPreferredSize(new Dimension(800, 100));
-                    JLabel title = LabelConfig.createLabel("    Shopping Cart", Style.FONT_TITLE_BOLD_40,Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, SwingConstants.LEFT,SwingConstants.CENTER);
-                    titlePnTop.add(title,BorderLayout.WEST);
+                    JLabel title = LabelConfig.createLabel("    Shopping Cart", Style.FONT_TITLE_BOLD_40, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, SwingConstants.LEFT, SwingConstants.CENTER);
+                    titlePnTop.add(title, BorderLayout.WEST);
 
-                    JLabel items = LabelConfig.createLabel("3 items     ", Style.FONT_BOLD_20,Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, SwingConstants.RIGHT,SwingConstants.CENTER);
+                    JLabel items = LabelConfig.createLabel("3 items     ", Style.FONT_BOLD_20, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, SwingConstants.RIGHT, SwingConstants.CENTER);
 
-                    titlePnTop.add(items,BorderLayout.EAST);
+                    titlePnTop.add(items, BorderLayout.EAST);
 
                     add(titlePnTop, BorderLayout.NORTH);
 
@@ -543,90 +543,117 @@ public class CustomerMainPanel extends JPanel {
                     scrollPane.setBackground(Color.WHITE);
                     scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
                     scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+                    scrollPane.setBorder(new MatteBorder(1, 0, 1, 0, Color.GRAY));
+
                     setColorScrollPane(scrollPane, Style.BACKGROUND_COLOR, Style.LIGHT_BlUE);
                     add(scrollPane, BorderLayout.CENTER);
 
                     // panel contain back button
-                    JPanel backPnBottom = new JPanel(new BorderLayout());
-                    backPnBottom.setBackground(Color.WHITE);
-                    backPnBottom.setPreferredSize(new Dimension(800, 100));
-                    backBt = ButtonConfig.createCustomButton("Back", Style.FONT_BOLD_16, Color.white, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE,
-                            Style.MEDIUM_BLUE, 5, SwingConstants.CENTER, new Dimension(110, 30));
-                    ButtonConfig.setButtonIcon("src/main/java/Icon/back_Icon1.png", backBt,5);
+                    JPanel BottomPn = new JPanel(new BorderLayout());
+                    BottomPn.setBackground(Color.WHITE);
+                    BottomPn.setPreferredSize(new Dimension(800, 80));
+                    JPanel backBtPn = new JPanel(new GridBagLayout());
+                    backBtPn.setBackground(Color.WHITE);
+                    backBt = ButtonConfig.createCustomButton("Back to shop", Style.FONT_BOLD_16, Color.white, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE,
+                            Style.MEDIUM_BLUE, 8, SwingConstants.CENTER, new Dimension(180, 35));
+                    ButtonConfig.setButtonIcon("src/main/java/Icon/back_Icon1.png", backBt, 5);
                     backBt.addActionListener(e -> {
                         ProductCatalogMainPanel.this.show("catalog");
                     });
+                    GridBagConstraints gbc = new GridBagConstraints();
+                    gbc.insets = new Insets(5, 30, 5, 5);
+                    gbc.gridy = 0;
+                    gbc.gridx = 0;
+                    backBtPn.add(backBt, gbc);
 
-                    backPnBottom.add(backBt, BorderLayout.WEST);
-                    add(backPnBottom, BorderLayout.SOUTH);
+                    BottomPn.add(backBtPn, BorderLayout.WEST);
+                    add(BottomPn, BorderLayout.SOUTH);
                 }
-
             }
 
             // thông tin người đặt mua và tính tiền
             class SummaryPanel extends JPanel {
-
-                private JLabel totalItem, totalPrice;
+                private JLabel totalItemLabel, totalPriceLabel;
                 private CustomButton orderBt;
 
                 SummaryPanel() {
-                    setLayout(new GridBagLayout());
-                    setPreferredSize(new Dimension(320, 300));
+                    setLayout(new BorderLayout());
+                    setPreferredSize(new Dimension(320, 100));
                     setBackground(Color.WHITE);
-                    GridBagConstraints gbc = new GridBagConstraints();
-                    gbc.insets = new Insets(5, 5, 5, 5);
-                    gbc.weightx = 1.0;
 
-                    JLabel title = new JLabel("Delivery Information");
-                    title.setFont(Style.FONT_BOLD_24);
-                    title.setForeground(Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE);
+
+                    JPanel titlePn = new JPanel(new BorderLayout());
+                    titlePn.setPreferredSize(new Dimension(320, 100));
+                    titlePn.setBackground(Color.WHITE);
+                    JLabel title = LabelConfig.createLabel("     Summary", Style.FONT_BOLD_30, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, SwingConstants.LEFT);
+                    titlePn.add(title, BorderLayout.CENTER);
+                    add(titlePn, BorderLayout.NORTH);
+
+                    JPanel summaryDetailsPn = new JPanel(new GridBagLayout());
+                    summaryDetailsPn.setBackground(Color.WHITE);
+                    GridBagConstraints gbc = new GridBagConstraints();
+                    gbc.insets = new Insets(0, 10, 10, 10);
+                    gbc.anchor = GridBagConstraints.WEST;
                     gbc.gridx = 0;
                     gbc.gridy = 0;
-                    add(title, gbc);
 
-                    emailField = TextFieldConfig.createTextField("", Style.FONT_PLAIN_18, Color.BLACK, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, new Dimension(300, 45));
+                    JSeparator separatorTop = new JSeparator(SwingConstants.HORIZONTAL);
+                    separatorTop.setPreferredSize(new Dimension(270, 5));
+                    summaryDetailsPn.add(separatorTop, gbc);
+
+
+                    JLabel emailLabel = LabelConfig.createLabel("EMAIL", Style.FONT_BOLD_20, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, SwingConstants.LEFT, SwingConstants.CENTER);
+                    gbc.insets = new Insets(10, 10, 10, 10);
+                    gbc.gridy++;
+                    summaryDetailsPn.add(emailLabel, gbc);
+
+                    emailField = TextFieldConfig.createTextField("", Style.FONT_PLAIN_18, Color.BLACK, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, new Dimension(270, 40));
                     emailField.setText(CurrentUser.CURRENT_CUSTOMER.getEmail());
-                    gbc.gridy = 1;
-                    add(emailField, gbc);
 
-                    nameField = TextFieldConfig.createTextField("", Style.FONT_PLAIN_18, Color.BLACK, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, new Dimension(300, 45));
+                    gbc.gridy++;
+                    summaryDetailsPn.add(emailField, gbc);
+
+                    JLabel nameLabel = LabelConfig.createLabel("NAME", Style.FONT_BOLD_20, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, SwingConstants.LEFT, SwingConstants.CENTER);
+
+                    nameField = TextFieldConfig.createTextField("", Style.FONT_PLAIN_18, Color.BLACK, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, new Dimension(270, 40));
                     nameField.setText(CurrentUser.CURRENT_CUSTOMER.getFullName());
-                    gbc.gridy = 2;
-                    add(nameField, gbc);
+
+                    gbc.gridy++;
+                    summaryDetailsPn.add(nameLabel, gbc);
+                    gbc.gridy++;
+                    summaryDetailsPn.add(nameField, gbc);
+
+                    JLabel addressLabel = LabelConfig.createLabel("SHIPPING ADDRESS", Style.FONT_BOLD_20, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, SwingConstants.LEFT, SwingConstants.CENTER);
 
 
-                    addressField = TextFieldConfig.createTextField("", Style.FONT_PLAIN_18, Color.BLACK, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, new Dimension(300, 45));
-                    addressField.setText(CurrentUser.CURRENT_CUSTOMER.getAddress());
-                    gbc.gridy = 4;
-                    add(addressField, gbc);
+                    addressField = TextFieldConfig.createTextField(CurrentUser.CURRENT_CUSTOMER.getAddress(),
+                            Style.FONT_PLAIN_18, Color.BLACK, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, new Dimension(270, 40));
 
-                    JSeparator separator = new JSeparator(SwingConstants.HORIZONTAL);
-                    separator.setOrientation(SwingConstants.HORIZONTAL);
+                    gbc.gridy++;
+                    summaryDetailsPn.add(addressLabel, gbc);
+                    gbc.gridy++;
+                    summaryDetailsPn.add(addressField, gbc);
 
-                    separator.setMaximumSize(new Dimension(Integer.MAX_VALUE, 2));
-                    gbc.gridy = 5;
-                    add(separator, gbc);
+                    JSeparator separatorMid = new JSeparator(SwingConstants.HORIZONTAL);
+                    separatorMid.setPreferredSize(new Dimension(270, 5));
+                    gbc.gridy++;
+                    summaryDetailsPn.add(separatorMid, gbc);
 
-                    JLabel cartTotal = new JLabel("Cart Total");
-                    cartTotal.setFont(Style.FONT_BOLD_24);
-                    cartTotal.setForeground(Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE);
-                    gbc.gridy = 6;
-                    add(cartTotal, gbc);
+                    totalItemLabel = new JLabel("<html> ITEMS <font color='green'>" + totalItemCount + "</font></html>");
+                    totalItemLabel.setFont(Style.FONT_BOLD_20);
+                    gbc.gridy++;
+                    summaryDetailsPn.add(totalItemLabel, gbc);
+
+                    String totalPrice = formatCurrency.format(999999999);
+                    totalPriceLabel = new JLabel("<html> TOTAL PRICE <font color='green'>" + totalPrice + "₫ </font></html>");
+
+                    totalPriceLabel.setFont(Style.FONT_BOLD_20);
+                    gbc.gridy++;
+                    summaryDetailsPn.add(totalPriceLabel, gbc);
 
 
-//                    totalItem = new JLabel(totalItemCount + " Items in your Cart");
-                    totalItem = new JLabel("<html> <font color='green'>" + totalItemCount + "</font> Items in your Cart</html>");
-                    totalItem.setFont(Style.FONT_BOLD_20);
-                    gbc.gridy = 7;
-                    add(totalItem, gbc);
-
-
-                    totalPrice = new JLabel("<html> <font color='black'>Total:  </font> <font color='red'>$" + totalPriceCount + "</font></html>");
-                    totalPrice.setFont(Style.FONT_BOLD_20);
-                    gbc.gridy = 8;
-                    add(totalPrice, gbc);
-
-                    orderBt = createCustomButtonGradientBorder("Order", Style.FONT_BOLD_30, new Color(14, 163, 204), Style.LIGHT_BlUE, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, Color.GREEN, 3, 15, new Dimension(250, 50));
+                    orderBt = createCustomButtonGradientBorder("Order", Style.FONT_BOLD_30, new Color(14, 163, 204),
+                            Style.LIGHT_BlUE, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, Color.GREEN, 3, 15, new Dimension(260, 50));
                     orderBt.setHorizontalAlignment(SwingConstants.CENTER);
                     orderBt.setAlignmentX(Component.CENTER_ALIGNMENT);
                     orderBt.addActionListener(new ActionListener() {
@@ -672,8 +699,13 @@ public class CustomerMainPanel extends JPanel {
                             }
                         }
                     });
-                    gbc.gridy = 9;
-                    add(orderBt, gbc);
+                    gbc.gridy++;
+                    gbc.anchor = GridBagConstraints.CENTER;
+                    summaryDetailsPn.add(orderBt, gbc);
+
+
+                    add(summaryDetailsPn, BorderLayout.CENTER);
+
                 }
             }
         }
@@ -711,7 +743,7 @@ public class CustomerMainPanel extends JPanel {
 
 
                 calendarBt = ButtonConfig.createCustomButton("", Style.FONT_PLAIN_20, Style.WORD_COLOR_WHITE, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, Style.LIGHT_BlUE, 0, SwingConstants.CENTER, new Dimension(50, 50));
-                ButtonConfig.setButtonIcon("src/main/java/Icon/calendarIcon.png", calendarBt,10);
+                ButtonConfig.setButtonIcon("src/main/java/Icon/calendarIcon.png", calendarBt, 10);
 
                 // Tạo JDialog chứa JCalendar
                 JDialog calendarDialog = new JDialog((Frame) null, "Select Date", true);
@@ -744,7 +776,7 @@ public class CustomerMainPanel extends JPanel {
 
                 searchField = TextFieldConfig.createTextField("Search Order", Style.FONT_PLAIN_20, Color.GRAY, new Dimension(350, 50));
                 searchBt = ButtonConfig.createCustomButton("", Style.FONT_PLAIN_20, Style.WORD_COLOR_WHITE, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, Style.LIGHT_BlUE, 0, SwingConstants.CENTER, new Dimension(60, 50));
-                ButtonConfig.setButtonIcon("src/main/java/Icon/search_Icon.png", searchBt,5);
+                ButtonConfig.setButtonIcon("src/main/java/Icon/search_Icon.png", searchBt, 5);
 
 
                 add(feedbackBt);
@@ -812,12 +844,11 @@ public class CustomerMainPanel extends JPanel {
             c.setAvataImg("src/main/java/img/837020177Screenshot 2024-10-20 134127.png");
             showFullBills(new BillConfig(bills).getMetadataMap(), c);
 
-
             searchField = TextFieldConfig.createTextField("Search Notification", Style.FONT_PLAIN_18, Color.GRAY, new Dimension(320, 40));
 
 
             searchButton = ButtonConfig.createCustomButton("", Style.FONT_PLAIN_20, Style.WORD_COLOR_WHITE, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE, Style.LIGHT_BlUE, 0, SwingConstants.CENTER, new Dimension(50, 40));
-            ButtonConfig.setButtonIcon("src/main/java/Icon/search_Icon.png", searchButton,5);
+            ButtonConfig.setButtonIcon("src/main/java/Icon/search_Icon.png", searchButton, 5);
 
             title.add(searchField);
             title.add(searchButton);
@@ -1278,7 +1309,7 @@ public class CustomerMainPanel extends JPanel {
 
         CustomButton backBt = ButtonConfig.createCustomButton("Back", Style.FONT_BOLD_16, Color.white, Style.LOGIN_FRAME_BACKGROUND_COLOR_BLUE,
                 Style.MEDIUM_BLUE, 5, SwingConstants.CENTER, new Dimension(110, 30));
-        ButtonConfig.setButtonIcon("src/main/java/Icon/back_Icon1.png", backBt,5);
+        ButtonConfig.setButtonIcon("src/main/java/Icon/back_Icon1.png", backBt, 5);
         backBt.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -1745,9 +1776,9 @@ public class CustomerMainPanel extends JPanel {
         proDetails.add(proBrand, gbc);
 
         gbc.gridy++;
-        JLabel proTechnicalDetail = LabelConfig.createLabel("CPU " + customerOrderDTO.customerOrderDTO().getCpu() +" / " +
-                                                            "RAM " + customerOrderDTO.customerOrderDTO().getRam() + " / " +
-                                                            "Storage " + customerOrderDTO.customerOrderDTO().getMemory(),
+        JLabel proTechnicalDetail = LabelConfig.createLabel("CPU " + customerOrderDTO.customerOrderDTO().getCpu() + " / " +
+                        "RAM " + customerOrderDTO.customerOrderDTO().getRam() + " / " +
+                        "Storage " + customerOrderDTO.customerOrderDTO().getMemory(),
                 Style.FONT_PLAIN_13, Color.BLACK, SwingConstants.LEFT);
         proDetails.add(proTechnicalDetail, gbc);
 
@@ -1757,12 +1788,12 @@ public class CustomerMainPanel extends JPanel {
         JPanel pricePn = new JPanel(new FlowLayout(FlowLayout.LEFT));// panel xem giá của 1 sản phẩm
         pricePn.setBackground(Color.WHITE);
         String unitPrice = formatCurrency.format(customerOrderDTO.customerOrderDTO().getUnitPrice());
-        JLabel proPrice = LabelConfig.createLabel( unitPrice+ "₫",Style.FONT_BOLD_18,Style.CONFIRM_BUTTON_COLOR_GREEN, SwingConstants.LEFT);
+        JLabel proPrice = LabelConfig.createLabel(unitPrice + "₫", Style.FONT_BOLD_18, Style.CONFIRM_BUTTON_COLOR_GREEN, SwingConstants.LEFT);
         pricePn.add(proPrice);
 
         JPanel quantityPn = new JPanel(new FlowLayout(FlowLayout.RIGHT));// panel xem số lượng mua của 1 sản phẩm
         quantityPn.setBackground(Color.WHITE);
-        JLabel proQuantity = LabelConfig.createLabel("x" + customerOrderDTO.customerOrderDTO().getQuantity(),Style.FONT_BOLD_18,Color.BLACK, SwingConstants.RIGHT);
+        JLabel proQuantity = LabelConfig.createLabel("x" + customerOrderDTO.customerOrderDTO().getQuantity(), Style.FONT_BOLD_18, Color.BLACK, SwingConstants.RIGHT);
         quantityPn.add(proQuantity);
 
         bottomPn.add(pricePn);
